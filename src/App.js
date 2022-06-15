4b0ee95ffca62c7d451da8b022a7fc9c2699d24b
Matias Bovina <matibovina@gmail.com>
2022-06-15 11:57:38 +0200

    changed header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import './App.css';
-import Variables from './components/Variables';
 
 function App() {
   return (
     <div className="App">
       <header className='App-header'>
-      <h1>Hello world!</h1>
-      <br/>
-      <a href='https://demo.bs.prefapp.es/' target='_blank' >Demo prefapp Backstage</a>
-      
-      <Variables />
+      ${{ values.header }}
       </header>
     </div>
   );
